27170be717c6679383b6d2e4617892f62dc1de63
jimmy1248 <jimmy1248@gmail.com>
2014-04-18 15:59:04 +1200

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-Otfeg-Teleports
-===============
+EasySpawnPlus
+=============
+
+EasySpawnPlus is an extension of [EasySpawn](http://dev.bukkit.org/bukkit-plugins/easy-spawn/) 
+giving more advanced teleportion features.
+This plugin is currently under development and new features will be added. 
+Feel free to request features and we will see what we can do to add them.
+
+Usage
+------
+/spawn
+
+	Teleports player to the spawn of the world they are in.
+
+/spawn list
+
+	List available spawns.
+	
+/spawn <world>
+ 
+	Teleports player to the spawn of the given world.
+	 
+/setspawn
+
+	Sets spawn location.
+
+/bed 
+	
+	Teleports player to their bed
+	
+Permissions
+-----------
+
+/spawn
+
+	easyspawn.spawn
+
+	by default this works for non-opped players 
+
+/setspawn
+
+	easyspawn.setspawn
+	
+	works if opped
+ 
+/bed
+
+    easyspawn.bed
+    
+    by default this works for non-opped players
+
+Configuration
+-------------
+
+delay: 
+	
+	The time delay between issuing a teleportation command and being teleported.
+	Set to 0 disable.
+	
+radius:
+    
+    Radius player must stay within, if they fail to do so teleport will fail.
+    If delay is disabled the radius check is not done.
+
+
+Source code be found [here](https://github.com/OTFEG/EasySpawnPlus) on github.
--- src/net/otfeg/easyspawnplus/commandhandlers/Spawn.java
+++ src/net/otfeg/easyspawnplus/commandhandlers/Spawn.java
@@ -1,7 +1,11 @@
 package net.otfeg.easyspawnplus.commandhandlers;
 
+import java.util.List;
+
 import net.otfeg.easyspawnplus.EasySpawnPlus;
 import net.otfeg.easyspawnplus.TeleportDelay;
+
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,9 +18,32 @@ public class Spawn implements CommandExecutor{
 	}
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
-		if(sender instanceof Player) {
-			Player player = (Player) sender;
-			new TeleportDelay(plugin, player , player.getWorld().getSpawnLocation(), plugin.getDelay(), plugin.getRadius(), "You have been teleported to spawn.");
+		if(!(sender instanceof Player)) return true;
+		Player player = (Player) sender;
+		// /spawn
+		if(args.length == 0){
+			new TeleportDelay(plugin, player , player.getWorld().getSpawnLocation(),
+					plugin.getDelay(), plugin.getRadius(),
+					"You have been teleported to spawn.");
+			return true;
+		}
+		if(args.length == 1){
+			if(args[0].equalsIgnoreCase("list")){ // /spawn list
+				List<World> worlds = plugin.getServer().getWorlds();
+				for(World world:worlds){
+					player.sendMessage(world.getName());
+				}
+			}else{ // /spawn <world>
+				World world = plugin.getServer().getWorld(args[0]);
+				if(world == null){ // check if work exist
+					player.sendMessage("World not found.");
+					player.sendMessage("Use /spawn list to show available worlds.");
+					return true;
+				}
+				new TeleportDelay(plugin, player, world.getSpawnLocation(),
+						plugin.getDelay(), plugin.getRadius(),
+						"You have been teleported to " + world.getName() + " spawn.");
+			}
 		}
 		return true;
 		
